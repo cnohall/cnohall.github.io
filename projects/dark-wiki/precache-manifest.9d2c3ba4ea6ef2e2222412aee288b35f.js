6e6249a2c30a6748925a41e6b2fb34b5e4b2a9a3
Christopher <christopher.nohall@gmail.com>
2019-10-28 16:46:28 +0800

    testing the dark wiki out

---
+++ projects/dark-wiki/precache-manifest.9d2c3ba4ea6ef2e2222412aee288b35f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "341c356cae55f86ca0878dec128496a5",
+    "url": "/dark-wiki/index.html"
+  },
+  {
+    "revision": "2a3a1a556d5db3a53ae4",
+    "url": "/dark-wiki/static/css/main.70e86af1.chunk.css"
+  },
+  {
+    "revision": "0d6778514972012a80e2",
+    "url": "/dark-wiki/static/js/2.cf422e5d.chunk.js"
+  },
+  {
+    "revision": "2a3a1a556d5db3a53ae4",
+    "url": "/dark-wiki/static/js/main.d442f5f9.chunk.js"
+  },
+  {
+    "revision": "911f3cd12607d55e579f",
+    "url": "/dark-wiki/static/js/runtime-main.bc8ab654.js"
+  }
+]);
